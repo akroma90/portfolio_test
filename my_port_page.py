32561d703949d26795ee439d2bc7ec654d82eaac
akroma <49003116+akroma90@users.noreply.github.com>
2024-01-11 21:24:30 +0300

    Update my_port_page.py

--- my_port_page.py
+++ my_port_page.py
@@ -275,5 +275,5 @@ def toggle_modal(n1, n2, is_open):
 
 
 
-#if __name__ == '__main__':
-#    app.run_server(debug=True,host='127.0.0.1',port=8050)
+if __name__ == '__main__':
+    app.run_server(debug=True,host='127.0.0.1',port=8050)
